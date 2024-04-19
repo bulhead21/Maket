168572fcccbba57e89c230ef176a0869ac3253dc
bulhead21 <147318634+bulhead21@users.noreply.github.com>
2024-04-19 22:35:14 +0500

    aaaa

--- static/js/likebtn.js
+++
@@ -1,4 +0,0 @@
-let icon = document.querySelector('ion-icon');
-icon.onclick = function(){
-  icon.classList.toggle('active');
-}
--- static/js/smooth.js
+++
@@ -1,7 +0,0 @@
-const container = {...document.querySelector('container').children};
-
-container.forEach(() => {
-  setTimeout(() => {
-    item.style.opacity = 1;
-  }, i*100);
-})
